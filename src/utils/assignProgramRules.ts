385c78b7eb9043f5cd4389209d00dc50fa14c281
davidbgomes <davidbgomes92@gmail.com>
2024-01-10 12:28:07 +0000

    fix bugs & handle rules for entire questionnaire

--- src/utils/assignProgramRules.ts
+++ src/utils/assignProgramRules.ts
@@ -19,21 +19,23 @@ export const assignProgramRules = (
         );
 
         filteredProgramRules?.map(rule => {
-            switch (rule?.programRuleActions[0]?.programRuleActionType) {
-                case "HIDEFIELD": {
-                    const questionToBeManipulated = sectionToBeUpdated.questions.find(
-                        q => q.id === rule?.programRuleActions[0]?.dataElement?.id
-                    );
-
-                    if (questionToBeManipulated) {
-                        questionToBeManipulated.isVisible = !parseCondition(
-                            rule.condition,
-                            updatedQuestion
+            rule.programRuleActions.map(action => {
+                switch (action.programRuleActionType) {
+                    case "HIDEFIELD": {
+                        const questionToBeManipulated = sectionToBeUpdated.questions.find(
+                            q => q.id === rule?.programRuleActions[0]?.dataElement?.id
                         );
+
+                        if (questionToBeManipulated) {
+                            questionToBeManipulated.isVisible = !parseCondition(
+                                rule.condition,
+                                updatedQuestion
+                            );
+                        }
+                        break;
                     }
-                    break;
                 }
-            }
+            });
         });
     }
 };
@@ -42,23 +44,25 @@ const parseCondition = (condition: string, updatedQuestion: Question): boolean =
     const regExLogicOperators = new RegExp("[&|]{2}", "g");
 
     const conditionArray = condition.split(regExLogicOperators);
+    const operatorsOrder = condition.match(regExLogicOperators);
 
-    let value: boolean | undefined;
-    conditionArray.forEach(condition => {
+    const values: boolean[] = [];
+    conditionArray.map(condition => {
         //If the condition is one of the d2Functions, handle them
         if (D2_FUNCTIONS.filter(d2Function => condition.includes(d2Function)).length !== 0) {
             // "TO DO: handle d2Functions"
+            values.push(false);
         } else {
             const operator = D2_OPERATORS.findLast(d2Operator =>
                 condition.includes(d2Operator)
             )?.trim();
-            if (!operator) throw new Error("Condition is wrongly built");
 
-            const leftOperand = isQuestionOptionValue(updatedQuestion.value)
+            if (!operator || !D2_OPERATORS.includes(operator))
+                throw new Error(`Operator ${operator} is either undefined or not handled`);
+
+            let leftOperand = isQuestionOptionValue(updatedQuestion.value)
                 ? updatedQuestion.value.code
                 : updatedQuestion.value;
-
-            if (leftOperand === undefined) throw new Error("Left operand is undefined");
 
             let rightOperand: string | boolean = condition
                 .substring(condition.indexOf(operator))
@@ -66,32 +70,41 @@ const parseCondition = (condition: string, updatedQuestion: Question): boolean =
                 .replaceAll("'", "")
                 .trim();
 
+            // Handle left operand boolean undefined, which means it's not filled, so false
+            // Handle right operands boolean values of "1" and "0" for true and false
             if (typeof updatedQuestion.value === "boolean") {
+                leftOperand = leftOperand === undefined ? false : leftOperand;
                 rightOperand = rightOperand === "1" ? true : false;
+            } else {
+                //If not boolean, means the option in the questionnaire is not filled, so condition is always false
+                if (leftOperand === undefined) {
+                    values.push(false);
+                }
             }
+
             switch (operator) {
                 case "!=": {
-                    value = leftOperand !== rightOperand;
+                    values.push(leftOperand !== rightOperand);
                     break;
                 }
                 case "==": {
-                    value = leftOperand === rightOperand;
+                    values.push(leftOperand === rightOperand);
                     break;
                 }
                 case ">": {
-                    value = leftOperand > rightOperand;
+                    if (leftOperand) values.push(leftOperand > rightOperand);
                     break;
                 }
                 case ">=": {
-                    value = leftOperand >= rightOperand;
+                    if (leftOperand) values.push(leftOperand >= rightOperand);
                     break;
                 }
                 case "<": {
-                    value = leftOperand < rightOperand;
+                    if (leftOperand) values.push(leftOperand < rightOperand);
                     break;
                 }
                 case "<=": {
-                    value = leftOperand <= rightOperand;
+                    if (leftOperand) values.push(leftOperand <= rightOperand);
                     break;
                 }
                 default:
@@ -100,15 +113,19 @@ const parseCondition = (condition: string, updatedQuestion: Question): boolean =
         }
     });
 
-    if (value !== undefined) {
-        return value;
+    //TO DO: handle multiple operations
+    const result: boolean | undefined =
+        operatorsOrder?.[0] === "&&" ? values.every(Boolean) : values.some(Boolean);
+
+    if (result !== undefined) {
+        return result;
     } else {
-        throw new Error("Error defining the conditions");
+        throw new Error("Program Rule could not be evaluated");
     }
 };
 
 const isQuestionOptionValue = (
     value: string | boolean | QuestionOption | Date | undefined
 ): value is QuestionOption => {
-    return (value as QuestionOption).code !== undefined;
+    return (value as QuestionOption)?.code !== undefined;
 };
