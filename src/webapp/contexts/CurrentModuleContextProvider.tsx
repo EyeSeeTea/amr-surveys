6303f59046133a374c5f86bfee9675cf56402059
davidbgomes <davidbgomes92@gmail.com>
2024-04-22 15:36:28 -0500

    persist currentModule context within page refresh

--- src/webapp/contexts/CurrentModuleContextProvider.tsx
+++ src/webapp/contexts/CurrentModuleContextProvider.tsx
@@ -1,12 +1,20 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { AMRSurveyModule } from "../../domain/entities/AMRSurveyModule";
 import { CurrentModuleContext } from "./current-module-context";
 
 export const CurrentModuleContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
     const [currentModule, setCurrentModule] = useState<AMRSurveyModule | undefined>();
 
+    useEffect(() => {
+        const module = window.sessionStorage.getItem("currentModule");
+        if (module) {
+            setCurrentModule(JSON.parse(module));
+        }
+    }, []);
+
     const changeCurrentModule = (module: AMRSurveyModule | undefined) => {
         setCurrentModule(module);
+        window.sessionStorage.setItem("currentModule", JSON.stringify(module));
     };
 
     const resetCurrentModule = () => {
