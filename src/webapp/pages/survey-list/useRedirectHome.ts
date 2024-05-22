40cbaec71c4918d4552d5059ac2ebfb5ac9274d2
9sneha-n <9sneha.n@gmail.com>
2024-05-22 18:54:41 +0530

    chore: translations

--- src/webapp/pages/survey-list/useRedirectHome.ts
+++ src/webapp/pages/survey-list/useRedirectHome.ts
@@ -9,7 +9,6 @@ export function useRedirectHome() {
     const {
         currentPPSSurveyForm,
         currentPrevalenceSurveyForm,
-        currentCountryQuestionnaire,
         currentHospitalForm,
         currentWardRegister,
         currentFacilityLevelForm,
