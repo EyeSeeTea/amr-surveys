1c3b6ffe696379b5eb087c1377163127fc99720c
xurxodev <xurxosf@gmail.com>
2025-05-19 11:06:44 +0200

    Refactor: simplify getDefaultProgram

--- src/data/utils/getDefaultProgram.ts
+++ src/data/utils/getDefaultProgram.ts
@@ -2,16 +2,9 @@ import { AMRSurveyModule } from "../../domain/entities/AMRSurveyModule";
 import { Id } from "../../domain/entities/Ref";
 
 export function getDefaultProgram(programId: Id, modules: AMRSurveyModule[]): string {
-    const hasCustomForms = (
-        module: AMRSurveyModule
-    ): module is AMRSurveyModule & { customForms: NonNullable<AMRSurveyModule["customForms"]> } =>
-        module.customForms !== undefined;
-
     const allCustomForms = modules
-        .filter(hasCustomForms)
-        .flatMap(module =>
-            Object.values(module.customForms).flatMap(forms => Object.entries(forms))
-        );
+        .flatMap(module => Object.values(module.customForms || {}))
+        .flatMap(forms => Object.entries(forms));
 
     return allCustomForms.find(entry => entry[1] === programId)?.[0] ?? programId;
 }
